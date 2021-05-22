0484b55baae0e5f84c93d285dffe88402cf406ad
Frantisek Varadzin <varadzin@gmail.com>
2021-05-22 18:05:51 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Frantisek Varadzin on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

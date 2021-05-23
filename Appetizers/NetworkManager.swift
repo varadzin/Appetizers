4dfd7d06c4852290fa6be9197d5b554a9e8c5373
Frantisek Varadzin <varadzin@gmail.com>
2021-05-23 15:58:17 +0200

    update

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -23,6 +23,32 @@ final class NetworkManager {
             return
         }
         
+        let task = URLSession.shared.dataTask(with: URLRequest (url: url))  { data, response, error in
+            guard let _ = error else {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+        
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+                completed(.success(decodedResponse.request))
+                
+            } catch {
+                completed(.failure(.invalidData))
+            }
+            
+            }
         
     }
     

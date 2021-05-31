aa72474e31968c8a21d2d20279cb1850c8305378
Frantisek Varadzin <varadzin@gmail.com>
2021-05-31 20:45:39 +0200

    update

---
+++ Appetizers/Screens/AppetizerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Frantisek Varadzin on 31/05/2021.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppetizerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerDetailView()
+    }
+}

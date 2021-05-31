4f0bf996f56a1daf630e1a31035e96976940f090
Frantisek Varadzin <varadzin@gmail.com>
2021-05-31 21:16:59 +0200

    update

--- Appetizers/Screens/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerDetailView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                
+        }
+        .frame(width: 320, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40
+        )
     }
 }
 
 struct AppetizerDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        AppetizerDetailView()
+        AppetizerDetailView(appetizer: MockData.sampleAppetizer)
     }
 }

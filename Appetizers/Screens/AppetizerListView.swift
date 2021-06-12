389a9983c1aa673e2bcc0e173db87d7544544372
Frantisek Varadzin <varadzin@gmail.com>
2021-06-12 14:39:19 +0200

    update

--- Appetizers/Screens/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct AppetizerListView: View {
     
     @StateObject var viewModel = AppetizerListViewModel()
-   @State private var isShowingDetail = false
-    @State private var selectedAppetizer : Appetizer?
+
     
     
     var body: some View {
@@ -22,22 +21,22 @@ struct AppetizerListView: View {
             List(viewModel.appetizers) { appetizer in
             AppetizerListCell(appetizer: appetizer)
                 .onTapGesture {
-                    selectedAppetizer = appetizer
-                    isShowingDetail = true
+                    viewModel.selectedAppetizer = appetizer
+                    viewModel.isShowingDetail = true
                 }
                 
             }.navigationTitle("Appetizers")
-            .disabled(isShowingDetail)
+            .disabled(viewModel.isShowingDetail)
             
         }.onAppear {
             viewModel.getAppetizers()
             
         }
-        .blur(radius: isShowingDetail ? 20 : 0) //ak je hodnota premennej isShowingDetail true tak da BLUR 20 ak  false tak BLUR je 0
+        .blur(radius: viewModel.isShowingDetail ? 20 : 0) //ak je hodnota premennej isShowingDetail true tak da BLUR 20 ak  false tak BLUR je 0
             
-            if isShowingDetail {
-                AppetizerDetailView(appetizer: selectedAppetizer!,
-                                    isShowingDetail: $isShowingDetail)
+            if viewModel.isShowingDetail {
+                AppetizerDetailView(appetizer: viewModel.selectedAppetizer!,
+                                    isShowingDetail: $viewModel.isShowingDetail)
             }
             
             if viewModel.isLoading {

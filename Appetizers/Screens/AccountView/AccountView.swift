5c131d015543d190b0ec90183a8e7344d80b11da
Frantisek Varadzin <varadzin@gmail.com>
2021-06-12 14:58:55 +0200

    update

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    
+    
     var body: some View {
         NavigationView {
-        Text("Account View")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    
+                }
+            
+                   
+            }
             .navigationTitle("Account")
         }
     }
@@ -19,5 +37,6 @@ struct AccountView: View {
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
+            
     }
 }

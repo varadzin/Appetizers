f86b47cc30ddcded04b56a7ec62ff95147e7cd4c
Frantisek Varadzin <varadzin@gmail.com>
2021-06-13 17:34:03 +0200

    update

--- Appetizers/Screens/AccountView/AccoutViewModel.swift
+++ Appetizers/Screens/AccountView/AccoutViewModel.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 final class AccoutViewModel: ObservableObject {
-
-@Published var firstName = ""
-@Published var lastName = ""
-@Published var email = ""
-@Published var birthdate = Date()
-@Published var extraNapkins = false
-@Published var frequentRefills = false
-
+    
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var email = ""
+    @Published var birthdate = Date()
+    @Published var extraNapkins = false
+    @Published var frequentRefills = false
+    @Published var alertItem: AlertItem?
+    
+    
     var isValidForm: Bool {
         guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else {
-            
+            alertItem = AlertContext.invalidForm
             return false
         }
         
         guard email.isValidEmail else {
-            
+            alertItem = AlertContext.invalidEmail
             return false
         }
         

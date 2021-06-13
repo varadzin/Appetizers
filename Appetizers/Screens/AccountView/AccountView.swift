63958009349eb63dbbc324fc355cf48d2dced459
Frantisek Varadzin <varadzin@gmail.com>
2021-06-13 10:07:15 +0200

    update

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -12,6 +12,9 @@ struct AccountView: View {
     @State private var firstName = ""
     @State private var lastName = ""
     @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
     
     
     var body: some View {
@@ -19,15 +22,30 @@ struct AccountView: View {
             Form {
                 Section(header: Text("Personal Info")) {
                     TextField("First Name", text: $firstName)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.words)
                     TextField("Last Name", text: $lastName)
+                        .disableAutocorrection(true)
+                        .autocapitalization(.words)
                     TextField("Email", text: $email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
-                    
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
                 }
             
-                   
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                      
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("Account")
         }
